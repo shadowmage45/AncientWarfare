0c0a55e9291ecb7cc87b8b25bd8614cd952f7501
shadowmage45 <shadowmage45@hotmail.com>
2013-02-06 16:40:58 -0700

    add offset adjustment for scanner BB render

--- AncientWarfare/src/shadowmage/ancient_warfare/client/render/AWRenderHelper.java
+++ AncientWarfare/src/shadowmage/ancient_warfare/client/render/AWRenderHelper.java
@@ -82,19 +82,54 @@ private void renderScannerBB(EntityPlayer player, ItemStack stack, ItemStructure
   BlockPosition p1 = item.getPos1(stack);
   if(p1==null)
     {
+    Config.logDebug("null scan pos1");
     return;
     }  
   int face = BlockTools.getPlayerFacingFromYaw(player.rotationYaw);
   //TODO test if offset is correct, or needed...possibly write another offset function
-  p1 = offsetForWorldRender(p1, face);
+  //p1 = offsetForWorldRender(p1, face);
   BlockPosition p2 = item.getPos2(stack);
   if(p2==null)
     {
     p2 = BlockTools.getBlockClickedOn(player, player.worldObj, player.isSneaking());
     }
-  p2 = offsetForWorldRender(p2, face);
+  if(p2==null)
+    {
+    Config.logDebug("null scan pos2");
+    return;
+    }
+  adjustPositionsForScanBB(p1, p2);
+  //p2 = offsetForWorldRender(p2, face);
   AxisAlignedBB bb = AxisAlignedBB.getBoundingBox(p1.x, p1.y, p1.z, p2.x, p2.y, p2.z);
   BoundingBoxRender.drawOutlinedBoundingBox(adjustBBForPlayerPos(bb, player, partialTick).contract(.02D, .02D, .02D), 0.8f, 0.2f, 0.8f);
+  }
+
+private void adjustPositionsForScanBB(BlockPosition p1, BlockPosition p2)
+  {
+  if(p2.x<p1.x)
+    {
+    p1.x++;
+    }
+  else
+    {
+    p2.x++;
+    }
+  if(p2.z<p1.z)
+    {
+    p1.z++;
+    }
+  else
+    {
+    p2.z++;
+    }
+  if(p1.y>p2.y)
+    {
+    p1.y++;
+    }
+  else
+    {
+    p2.y++;
+    }
   }
 
 private void renderStructureBB(EntityPlayer player, ItemStack stack, ItemBuilderBase item, float partialTick)
