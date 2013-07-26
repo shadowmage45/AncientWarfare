98fb8a730cb91ffb556e991b61902881e165532b
shadowmage45 <shadowmage45@hotmail.com>
2013-07-25 20:24:56 -0600

    * update version number and disable redstone stuff for release

--- AncientWarfare/src/shadowmage/ancient_warfare/common/config/Config.java
+++ AncientWarfare/src/shadowmage/ancient_warfare/common/config/Config.java
@@ -36,14 +36,14 @@ import shadowmage.ancient_warfare.common.npcs.NpcTypeBase;
 public class Config
 {
 //*******************************************************FIELDS**********************************************//
-public static final String VERSION = "1.0.015-alpha-MC152";//major version(mc version updates/major revisions), minor version(releases), build version(test releases total)
+public static final String VERSION = "1.0.016-alpha-MC152";//major version(mc version updates/major revisions), minor version(releases), build version(test releases total)
 
 public static String texturePath = "/mods/ancientwarfare/textures/";
 
 /**
  * should debug features be enabled? (debug keybinds, debug overlay rendering, load and enable debug items)
  */
-public static final boolean DEBUG = true;
+public static final boolean DEBUG = false;
 
 
 //***************************************************LOADED CONFIGS******************************************//
@@ -267,6 +267,7 @@ private void setStructureInfo()
     config.get("structure-management", "exportdefaults", false).set(false);//.value = "false";
     AWStructureModule.instance().setExportDefaults();
     }  
+  this.enableVillageGen = config.get("structure-management", "enableVillageStructures", true, "If true, will generate additional Ancient Warfare structures in villages").getBoolean(true);
   }
 
 public void handleClientInit(NBTTagCompound tag)
