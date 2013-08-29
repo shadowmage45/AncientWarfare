2521d07c0c4267307391e7068dd8f1bc3a31ec58
shadowmage45 <shadowmage45@hotmail.com>
2013-08-28 18:55:28 -0600

    * minor updates to BlockPosition

--- AncientWarfare/src/shadowmage/ancient_warfare/common/utils/BlockPosition.java
+++ AncientWarfare/src/shadowmage/ancient_warfare/common/utils/BlockPosition.java
@@ -262,5 +262,35 @@ public void offsetForMCSide(int side)
   }
   }
 
+@Override
+public int hashCode()
+  {
+  final int prime = 31;
+  int result = 1;
+  result = prime * result + x;
+  result = prime * result + y;
+  result = prime * result + z;
+  return result;
+  }
+
+@Override
+public boolean equals(Object obj)
+  {
+  if (this == obj)
+    return true;
+  if (obj == null)
+    return false;
+  if (getClass() != obj.getClass())
+    return false;
+  BlockPosition other = (BlockPosition) obj;
+  if (x != other.x)
+    return false;
+  if (y != other.y)
+    return false;
+  if (z != other.z)
+    return false;
+  return true;
+  }
+
 }
 
