a35c35c04ad153a305ce6df24b27993d21689988
shadowmage45 <shadowmage45@hotmail.com>
2013-08-28 18:41:29 -0600

    * update build script to not include buildcraft files after reobf * update included village_ structures to be in survival mode by default

--- build.bat
+++ build.bat
@@ -13,7 +13,7 @@ echo RECOMPILE COMPLETED, REOBFUSCATING.....
 runtime\bin\python\python_mcp runtime\reobfuscate.py %*
 cd..
 echo COPYING COMPILED FILES FOR REPACKAGE.......
-xcopy /e/i/q/y "%STARTPATH%\mcp\reobf\minecraft\*.*" "%STARTPATH%\build\*.*"
+xcopy /e/i/q/y "%STARTPATH%\mcp\reobf\minecraft\shadowmage\*.*" "%STARTPATH%\build\shadowmage\*.*"
 xcopy /e/i/q/y "%STARTPATH%\AncientWarfare\src\shadowmage\ancient_warfare\resources\*.*" "%STARTPATH%\build\shadowmage\ancient_warfare\resources\*.*"
 xcopy /e/i/q/y "%STARTPATH%\AncientWarfare\src\mods\*.*" "%STARTPATH%\build\mods\*.*"
 xcopy /e/i/q/y "%STARTPATH%\AncientWarfare\src\lang\*.*" "%STARTPATH%\build\lang\*.*"
