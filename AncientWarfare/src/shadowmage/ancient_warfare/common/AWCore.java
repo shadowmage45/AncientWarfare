d928396adf2192c60e0a786cb969f0cc2792b665
shadowmage45 <shadowmage45@hotmail.com>
2013-09-03 11:03:54 -0600

    * fix a potential cause of loss of research data on dedicated servers

--- AncientWarfare/src/shadowmage/ancient_warfare/common/AWCore.java
+++ AncientWarfare/src/shadowmage/ancient_warfare/common/AWCore.java
@@ -25,7 +25,6 @@ package shadowmage.ancient_warfare.common;
 
 import java.io.IOException;
 
-import net.minecraft.src.ModLoader;
 import net.minecraftforge.common.ForgeChunkManager;
 import net.minecraftforge.common.MinecraftForge;
 import shadowmage.ancient_warfare.common.block.BlockLoader;
@@ -63,12 +62,12 @@ import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.Mod.PostInit;
 import cpw.mods.fml.common.Mod.PreInit;
-import cpw.mods.fml.common.Mod.ServerStopping;
+import cpw.mods.fml.common.Mod.ServerStarting;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.event.FMLServerStoppingEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -209,8 +208,8 @@ public void load(FMLPostInitializationEvent evt)
   Config.log("Ancient Warfare Post-Init completed.  Successfully completed all loading stages."); 
   }
 
-@ServerStopping
-public void serverStarting(FMLServerStoppingEvent evt)
+@ServerStarting
+public void serverStarting(FMLServerStartingEvent evt)
   {
   GameDataTracker.instance().resetAllTrackedData();
   }
