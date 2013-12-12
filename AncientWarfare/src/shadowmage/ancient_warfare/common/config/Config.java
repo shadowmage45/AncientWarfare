88e903a71794e00a9c1b0a728ac662143680b924
shadowmage45 <shadowmage45@hotmail.com>
2013-12-11 18:12:42 -0700

    update version number, add 037 releases

--- AncientWarfare/src/shadowmage/ancient_warfare/common/config/Config.java
+++ AncientWarfare/src/shadowmage/ancient_warfare/common/config/Config.java
@@ -36,7 +36,7 @@ import shadowmage.ancient_warfare.common.npcs.NpcTypeBase;
 public class Config
 {
 //*******************************************************FIELDS**********************************************//
-public static final String VERSION = "1.1.036-beta-MC164";//major version(mc version updates/major revisions), minor version(releases), build version(test releases total)
+public static final String VERSION = "1.1.037-beta-MC164";//major version(mc version updates/major revisions), minor version(releases), build version(test releases total)
 public static final String ANTVERSION = "@VERSION@";
 public static String texturePath = "textures/";
 public static String configPath = "";
