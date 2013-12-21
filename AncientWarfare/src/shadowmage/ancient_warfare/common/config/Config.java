684300a81051ed7fdde9948fcba0dad1be8ffab2
shadowmage45 <shadowmage45@hotmail.com>
2013-12-21 11:23:47 -0700

    cleanup loot gen issue.  update version #. add missing file from last commit

--- AncientWarfare/src/shadowmage/ancient_warfare/common/config/Config.java
+++ AncientWarfare/src/shadowmage/ancient_warfare/common/config/Config.java
@@ -36,7 +36,7 @@ import shadowmage.ancient_warfare.common.npcs.NpcTypeBase;
 public class Config
 {
 //*******************************************************FIELDS**********************************************//
-public static final String VERSION = "1.0.037-beta-MC152";//major version(major revision).minor version(mc updates).build_version(the actual indiv release)-build state (beta)-MC version-automated build number (optional)
+public static final String VERSION = "1.0.038-beta-MC152";//major version(major revision).minor version(mc updates).build_version(the actual indiv release)-build state (beta)-MC version-automated build number (optional)
 public static final String ANTVERSION = "@VERSION@";
 public static String texturePath = "/mods/ancientwarfare/textures/";
 public static String configPath = "";
@@ -44,7 +44,7 @@ public static String configPath = "";
 /**
  * should debug features be enabled? (debug keybinds, debug overlay rendering, load and enable debug items)
  */
-public static final boolean DEBUG = true;
+public static final boolean DEBUG = false;
 
 //***************************************************LOADED CONFIGS******************************************//
 
