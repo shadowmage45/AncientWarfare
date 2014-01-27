09bbbd20515c7f78836b20e1a7c8f57ff969ac1c
shadowmage45 <shadowmage45@hotmail.com>
2014-01-27 15:39:50 -0700

    more work on model layout/format.

--- AncientWarfare/src/shadowmage/ancient_vehicles/client/model/ModelBaseAW.java
+++ AncientWarfare/src/shadowmage/ancient_vehicles/client/model/ModelBaseAW.java
@@ -43,27 +43,35 @@ public void renderModel()
     }
   }
 
-public void parseFromLines(List<String> lines){}
+public void parseFromLines(List<String> lines)
+  {
+  
+  }
 
 protected void addPiece(ModelPiece piece)
   {
   pieces.put(piece.pieceName, piece);
-  if(piece.parent==null)
+  if(piece.isBasePiece)
     {
     basePieces.add(piece);
     }
+  }
+
+protected ModelPiece getPiece(String name)
+  {
+  return this.pieces.get(name);
   }
 
 private static class ModelPiece
 {
 
 String pieceName;
-float x, y, z;
-float rx, ry, rz;
-int displayListNum = -1;
-private ModelPiece parent;
-private List<ModelPiece> children = new ArrayList<ModelPiece>();
-private List<Box> boxes = new ArrayList<Box>();
+float x, y, z;//manipulatable coordinates for this piece
+float rx, ry, rz;//manipulatable rotation for this piece
+int displayListNum = -1;//display list for the boxes that make up this piece
+private boolean isBasePiece;//if this is a base-piece or not
+private List<ModelPiece> children = new ArrayList<ModelPiece>();//the children of this piece
+private List<Box> boxes = new ArrayList<Box>();//the list of boxes that make up this piece, really only used during first construction of display list
 
 public void render()
   {
@@ -109,7 +117,7 @@ private static class Box
 
 float x1, y1, z1, x2, y2, z2;//extents of the box
 float rx, ry, rz;
-float tx, ty, tz;//texture offsets, in texture space (0->1)
+float tx, ty;//texture offsets, in texture space (0->1)
 
 private void render()
   {
@@ -119,8 +127,50 @@ private void render()
   if(ry!=0){GL11.glRotatef(ry, 0, 1, 0);}
   if(rz!=0){GL11.glRotatef(rz, 0, 0, 1);}  
   
-  //add vertices for cube
-  //with texture vertices
+  //front side
+  GL11.glBegin(GL11.GL_QUADS);
+  GL11.glTexCoord2f(tx, ty);//offset for the coords for the 'front' face
+  GL11.glVertex3f(x1, y1, z1);
+  GL11.glVertex3f(x2, y1, z1);
+  GL11.glVertex3f(x2, y2, z1);
+  GL11.glVertex3f(x1, y2, z1);
+  
+  //right side
+  GL11.glTexCoord2f(tx, ty);//offset for the coords for the 'right' face
+  GL11.glVertex3f(x2, y1, z1);
+  GL11.glVertex3f(x2, y1, z2);
+  GL11.glVertex3f(x2, y2, z2);
+  GL11.glVertex3f(x2, y2, z1);
+  
+  //left side
+  GL11.glTexCoord2f(tx, ty);//offset for the coords for the 'left' face
+  GL11.glVertex3f(x2, y1, z2);
+  GL11.glVertex3f(x2, y1, z1);
+  GL11.glVertex3f(x2, y2, z1);
+  GL11.glVertex3f(x2, y2, z2);
+
+  //top side
+  GL11.glTexCoord2f(tx, ty);//offset for the coords for the 'top' face
+  GL11.glVertex3f(x1, y1, z2);
+  GL11.glVertex3f(x2, y1, z2);
+  GL11.glVertex3f(x2, y1, z1);
+  GL11.glVertex3f(x1, y1, z1);
+  
+  //bottom side
+  GL11.glTexCoord2f(tx, ty);//offset for the coords for the 'bottom' face
+  GL11.glVertex3f(x1, y2, z1);
+  GL11.glVertex3f(x2, y2, z1);
+  GL11.glVertex3f(x2, y2, z2);
+  GL11.glVertex3f(x1, y2, z2);
+  
+  //rear side
+  GL11.glTexCoord2f(tx, ty);//offset for the coords for the 'rear' face
+  GL11.glVertex3f(x2, y1, z2);
+  GL11.glVertex3f(x1, y1, z2);
+  GL11.glVertex3f(x1, y2, z2);
+  GL11.glVertex3f(x2, y2, z2);
+  
+  GL11.glEnd();
   GL11.glPopMatrix();  
   }
 
