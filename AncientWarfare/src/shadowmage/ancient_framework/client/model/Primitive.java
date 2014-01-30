4f33f354ced15946957dc90b83fc8e36be230c77
shadowmage45 <shadowmage45@hotmail.com>
2014-01-30 12:51:22 -0700

    add fields and methods to Primitive for origin, rotation, position

--- AncientWarfare/src/shadowmage/ancient_framework/client/model/Primitive.java
+++ AncientWarfare/src/shadowmage/ancient_framework/client/model/Primitive.java
@@ -23,14 +23,59 @@ package shadowmage.ancient_framework.client.model;
 public abstract class Primitive
 {
 
+float x, y, z;//origin of this primitive, relative to parent origin and orientation
+
+float rx, ry, rz;//rotation of this primitive, relative to parent orientation
+
+float x1, y1, z1, x2, y2, z2;//extends of bounding box in local space (post rotation) -- used for w/l/h for boxes
+
 int primitiveNumber = 0;
+
 public ModelPiece parent;
+
 public Primitive(ModelPiece parent)
   {
   this.parent = parent;
   }
 
 public abstract void render();
+
 public abstract Primitive copy();
 
+public float x(){return x;}
+public float y(){return y;}
+public float z(){return z;}
+public float rx(){return rx;}
+public float ry(){return ry;}
+public float rz(){return rz;}
+public float x1(){return x1;}
+public float y1(){return y1;}
+public float z1(){return z1;}
+public float x2(){return x2;}
+public float y2(){return y2;}
+public float z2(){return z2;}
+
+public void setOrigin(float x, float y, float z)
+  {
+  this.x = x;
+  this.y = y;
+  this.z = z;
+  }
+
+public void setRotation(float rx, float ry, float rz)
+  {
+  this.rx = rx;
+  this.ry = ry;
+  this.rz = rz;
+  }
+
+public void setBounds(float x1, float y1, float z1, float x2, float y2, float z2)
+  {
+  this.x1 = x1;
+  this.x2 = x2;
+  this.y1 = y1;
+  this.y2 = y2;
+  this.z1 = z1;
+  this.z2 = z2;
+  }
 }
