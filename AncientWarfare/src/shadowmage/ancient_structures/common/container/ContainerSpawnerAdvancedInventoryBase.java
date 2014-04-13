e0bfb171fbb904c0da8381d4002cc62f2f59ae7f
shadowmage45 <shadowmage45@hotmail.com>
2014-04-12 20:40:40 -0600

    fix crash in spawner-advanced rewards inventory on shift-click item-merge (silly default container code...)

--- AncientWarfare/src/shadowmage/ancient_structures/common/container/ContainerSpawnerAdvancedInventoryBase.java
+++ AncientWarfare/src/shadowmage/ancient_structures/common/container/ContainerSpawnerAdvancedInventoryBase.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 import java.util.List;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -58,6 +59,47 @@ protected void addSettingsInventorySlots()
       addSlotToContainer(new SlotFiltered(inventory, slotNum, xPos, yPos, filter));
       }
     }
+  }
+
+@Override
+public ItemStack transferStackInSlot(EntityPlayer par1EntityPlayer, int slotClickedIndex)
+  {
+  ItemStack slotStackCopy = null;
+  Slot theSlot = (Slot)this.inventorySlots.get(slotClickedIndex);
+  if (theSlot != null && theSlot.getHasStack())
+    {
+    ItemStack slotStack = theSlot.getStack();
+    slotStackCopy = slotStack.copy();
+    int storageSlots = 9;  
+    if (slotClickedIndex < 36)//player slots...
+      {      
+      if (!this.mergeItemStack(slotStack, 36, 36+storageSlots, false))//merge into storage inventory
+        {
+        return null;
+        }
+      }
+    else if(slotClickedIndex >=36 &&slotClickedIndex < 36+storageSlots)//storage slots, merge to player inventory
+      {
+      if (!this.mergeItemStack(slotStack, 0, 36, true))//merge into player inventory
+        {
+        return null;
+        }
+      }
+    if (slotStack.stackSize == 0)
+      {
+      theSlot.putStack((ItemStack)null);
+      }
+    else
+      {
+      theSlot.onSlotChanged();
+      }
+    if (slotStack.stackSize == slotStackCopy.stackSize)
+      {
+      return null;
+      }
+    theSlot.onPickupFromSlot(par1EntityPlayer, slotStack);
+    }
+  return slotStackCopy;
   }
 
 public void sendSettingsToServer()
